1e5f8768913f3da985972cfbd7c8f07bd483f021
renardeinside <polarpersonal@gmail.com>
2024-07-01 23:07:54 +0200

    first working version

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx(), svelte()],
+  site: 'https://renardeinside.github.io',
+});
